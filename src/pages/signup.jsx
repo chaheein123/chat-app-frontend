bc4ee2f8609ba04ef5e0da45830bed470c830d5e
DJ Kim <djkim@paloaltonetworks.com>
2019-12-24 16:18:41 -0800

    Set up react router, react bootstrap, and react hook form

---
+++ src/pages/signup.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+
+import "./signup.css";
+
+export default function Signup(props) {
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = data => {
+    console.log(data);
+  };
+
+  return (
+    <div className="signup">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Group controlId="email" bsSize="large">
+          <Form.Label>Email</Form.Label>
+          <Form.Control autoFocus type="email" name="email" ref={register} />
+        </Form.Group>
+        <Form.Group controlId="password" bsSize="large">
+          <Form.Label>Password</Form.Label>
+          <Form.Control name="passowrd" ref={register} type="password" />
+        </Form.Group>
+        <Button block bsSize="large" type="submit">
+          Login
+        </Button>
+      </form>
+    </div>
+  );
+}
