0bb5923490b69c5db464e20a27d4742fb53d070e
Ian Cha <chaheein@gmail.com>
2020-01-30 18:53:38 -0800

    before going to dj

--- src/components/Friends/FriendsLeft/FriendsSearchOption/FriendsSearchOption.jsx
+++ src/components/Friends/FriendsLeft/FriendsSearchOption/FriendsSearchOption.jsx
@@ -62,14 +62,37 @@ class FriendsSearchOption extends React.Component {
           </div>
 
           <div className="search-option-username">
+
             {
-              this.props.username ?
+              this.props.username.length ?
                 <span className="search-option-username-span">
-                  <strong className="search-option-username-span">{this.props.username}</strong> ({this.props.useremail})
+                  <strong className="search-option-username-span">
+                    {this.props.username}
+                  </strong>
+
+                  {
+                    this.props.useremail.length > 17 ?
+                      this.props.useremail.substring(0, 8) + "..." + this.props.useremail.substring(this.props.useremail.length - 10, this.props.useremail.length + 1)
+                      :
+                      this.props.useremail
+
+                  }
+                  {/* ({this.props.useremail}) */}
                 </span>
                 :
-                <strong className="search-option-username-span">{this.props.useremail}</strong>
+                <strong className="search-option-username-span">
+
+                  {
+                    this.props.useremail.length > 17 ?
+                      this.props.useremail.substring(0, 8) + "..." + this.props.useremail.substring(this.props.useremail.length - 10, this.props.useremail.length + 1)
+                      :
+                      this.props.useremail
+                  }
+
+
+                </strong>
             }
+
           </div>
 
           {
@@ -82,8 +105,7 @@ class FriendsSearchOption extends React.Component {
           }
 
           {
-            this.state.sentRequest
-              ?
+            this.state.sentRequest ?
               <div
                 className="search-option-request clickstay"
                 onClick={
@@ -93,23 +115,27 @@ class FriendsSearchOption extends React.Component {
                 <p className="clickstay">Click to cancel friend request</p>
               </div>
               :
-              <div
-                className="search-option-not-request clickstay"
-                onClick={
-                  this.sendRequest.bind(null, this.props.useremail)
-                }
-              >
-                <p className="clickstay">Click to send friend request</p>
-              </div>
-          }
-          {
-            this.state.receivedRequest
-              ?
-              <div className="search-option-requestreceived clickstay">
-                request received
-              </div>
-              :
-              null
+              this.state.receivedRequest ?
+                <div className="search-option-requestreceived clickstay">
+                  <div className="search-option-requestreceived-flex">
+                    <p>blah has sent you friend request</p>
+                    <div>
+                      <button className="request-button request-button-accept">Accept</button>
+                      <button className="request-button request-button-decline">Decline</button>
+                    </div>
+                  </div>
+
+
+                </div>
+                :
+                <div
+                  className="search-option-not-request clickstay"
+                  onClick={
+                    this.sendRequest.bind(null, this.props.useremail)
+                  }
+                >
+                  <p className="clickstay">Click to send friend request</p>
+                </div>
           }
         </div>
       </div>
